fe6802554a87e37e19e2e674789bb1d03c648e02
Jaedan Seabaugh <janse7155@gmail.com>
2023-08-29 14:36:42 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const userName = prompt("Enter your name: ");
+
+console.log("Hello " + userName + "! Welcome to the console!" );
+
+console.log("Hello, World!");
+
+const name  = 'Jaedan';
+console.log(name);
+
+const length = 4;
+const width = 8;
+console.log(length * width + ' is the area of a rectangle');
+
+let number = 6;
+if (number % 2 === 0) {
+    console.log(number + ' is even');
+  } else {
+    console.log(number + ' is odd');
+  }
+
+  for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
